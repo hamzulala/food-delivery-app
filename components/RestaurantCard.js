ef7fa37cc6dbd1cefed7b5f33add04c98e0947ea
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-02-04 00:25:54 +1100

    Created Restaurant Screen
    - Created RestaurantScreen Component
    - Added UI Elements for RestaurantScreen Components

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,10 +3,14 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid'
 import { MapPinIcon } from 'react-native-heroicons/outline'
 import { urlFor } from '../sanity'
+import { useNavigation } from '@react-navigation/core'
 
 const RestaurantCard = ({id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat}) => {
+
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity className='bg-white mr-3 shadow'>
+    <TouchableOpacity onPress={() => {navigation.navigate('Restaurant', {id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat})}} className='bg-white mr-3 shadow'>
       <Image source={{uri: urlFor(imgUrl).url(),}} className='h-36 w-64 rounded-sm'/>
       <View className='px-3 pb-4'>
         <Text className='font-bold text-lg pt-2'>{title}</Text>
