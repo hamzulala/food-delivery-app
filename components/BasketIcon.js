d77302de51a6c7093d2d544070a1ace18e56285d
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-02-06 13:50:19 +1100

    Built Basket Screen
    - Built Basket Screen to include dishes, subtotal, and order total
    - finished UI for Basket Screen
    - created restaurantSlice so i can get dishes details and counts
    - Implemented restaurantSlice to store

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -9,12 +9,14 @@ const BasketIcon = () => {
     const navigation = useNavigation()
     const basketTotal = useSelector(selectBasketTotal)
 
+    if ( items.length === 0 ) return null
+
   return (
     <View className='absolute bottom-10 w-full z-50 p-4'>
-      <TouchableOpacity className='bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1'>
+      <TouchableOpacity onPress={() => navigation.navigate('Basket')} className='bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1'>
         <Text className='text-white font-extrabold text-lg bg-[#01A296] py-1 px-2'>{items.length}</Text>
         <Text className='flex-1 text-white font-extrabold text-lg text-center'>View Basket</Text>
-        <Text className='text-lg text-white font-extrabold'>${basketTotal}</Text>
+        <Text className='text-lg text-white font-extrabold'>${parseFloat(basketTotal).toFixed(2)}</Text>
       </TouchableOpacity>
     </View>
   )
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,15 +1,22 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/core'
 import { urlFor } from '../sanity'
 import { ArrowLeftIcon, ChevronRightIcon, MapPinIcon, StarIcon } from 'react-native-heroicons/solid'
 import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline'
 import DishRow from '../components/DishRow'
 import BasketIcon from '../components/BasketIcon'
+import { useDispatch } from 'react-redux'
+import { setRestaurant } from '../features/restaurantSlice'
 
 const RestaurantScreen = () => {
     const navigation = useNavigation()
+    const dispatch = useDispatch()
     const {params: {id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat}} = useRoute() 
+
+    useEffect(() => {
+        dispatch(setRestaurant({ id, imgUrl, title, rating, genre, address, short_description, dishes, long, lat }))
+    }, [dispatch])
     
     useLayoutEffect(() => {
         navigation.setOptions({
